7cad46f2fee94522422f2eb7c13c028b7f69bd4c
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-10-31 14:49:24 +0545

    Update vercel.json

--- dist/utils/random.js
+++ dist/utils/random.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.randint = randint;
+exports.randint = void 0;
 function randint(start, end) {
     if ((end - start) <= 0)
         throw new Error("Invalid domain");
     return Math.floor(start + (end - start + 1) * Math.random());
 }
+exports.randint = randint;
